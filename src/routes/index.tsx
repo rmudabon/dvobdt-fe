08d2c72c27f691210d5e5d646609a2bd0c228b92
imkoe <rmdabs@gmail.com>
2026-04-19 22:00:56 +0800

    feat: change color

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { PinMap } from '@/components/map/PinMap'
 import { MapPin } from 'lucide-react'
-import { PIN_COLOR } from '@/utils/constants'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -11,12 +10,12 @@ function App() {
   return (
     <div className='p-8 flex gap-8 h-full flex-col md:flex-row'>
       <div className='basis-xl flex flex-col justify-center gap-4 max-md:flex-1'>
-        <h1 className='text-3xl text-teal-700 font-semibold'>dvobdt</h1>
-        <div className='border rounded-md border-teal-700 p-4 space-y-4'>
+        <h1 className='text-3xl text-primary font-semibold'>dvobdt</h1>
+        <div className='border rounded-md border-primary p-4 space-y-4'>
           <h2 className='text-xl font-medium'>Nearest Bidets</h2>
           <ul className='text-lg space-y-2'>
-            <li className='flex items-center gap-2'><MapPin color={PIN_COLOR}/><Link to='/' className='hover:text-teal-700 transition-colors'>SM Lanang</Link></li>
-            <li className='flex items-center gap-2'><MapPin color={PIN_COLOR}/><Link to='/' className='hover:text-teal-700 transition-colors'>Abreeza Mall</Link></li>
+            <li className='flex items-center gap-2'><MapPin className='text-primary'/><Link to='/' className='hover:text-primary transition-colors'>SM Lanang</Link></li>
+            <li className='flex items-center gap-2'><MapPin className='text-primary'/><Link to='/' className='hover:text-primary transition-colors'>Abreeza Mall</Link></li>
           </ul>
         </div>
       </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,1 @@
 export const API_URL = 'https://dvobdt.com/api'
-
-export const PIN_COLOR = 'oklch(51.1% 0.096 186.391)'
