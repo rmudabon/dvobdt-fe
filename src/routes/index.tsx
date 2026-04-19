437945101a729020232b879bb5a956dce599b0e4
imkoe <rmdabs@gmail.com>
2026-04-19 22:28:36 +0800

    feat: responsive layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,18 +8,19 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className='p-8 flex gap-8 h-full flex-col md:flex-row bg-secondary'>
-      <div className='basis-xl flex flex-col justify-center gap-4 max-md:flex-1'>
+    <div className='p-8 flex gap-8 h-full flex-col xl:flex-row bg-secondary'>
+      <div className='flex flex-col justify-center gap-4 xl:basis-xl'>
         <h1 className='text-3xl text-primary font-semibold'>dvobdt</h1>
         <div className='border rounded-md border-primary p-4 space-y-4'>
           <h2 className='text-xl font-medium'>Nearest Bidets</h2>
           <ul className='text-lg space-y-2'>
             <li className='flex items-center gap-2'><MapPin className='text-primary'/><Link to='/' className='hover:text-primary transition-colors'>SM Lanang</Link></li>
             <li className='flex items-center gap-2'><MapPin className='text-primary'/><Link to='/' className='hover:text-primary transition-colors'>Abreeza Mall</Link></li>
+            <li className='flex items-center gap-2'><MapPin className='text-primary'/><Link to='/' className='hover:text-primary transition-colors'>GMall of Davao</Link></li>
           </ul>
         </div>
       </div>
-      <div className='flex-1 max-md:hidden'>
+      <div className='flex-1'>
         <PinMap />
       </div>
     </div>
