042726b1952ff9f6557c207c6b41f68a7d56fddb
Your Name <you@example.com>
2026-05-30 15:55:28 +0800

    fix: build error and vite URL

--- src/hooks/useLocations.ts
+++ src/hooks/useLocations.ts
@@ -1,29 +1,29 @@
-import { fetchLocationDetail, fetchLocations } from "@/services/locations"
-import { useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query";
+import { fetchLocationDetail, fetchLocations } from "@/services/locations";
 
 export const useLocations = () => {
-    const query = useQuery({
-        queryKey: ['locations'],
-        queryFn: fetchLocations,
-    })
+	const query = useQuery({
+		queryKey: ["locations"],
+		queryFn: () => fetchLocations(),
+	});
 
-    return query
-}
+	return query;
+};
 
 export const useMyLocations = () => {
-    const query = useQuery({
-        queryKey: ['locations', 'mine'],
-        queryFn: () => fetchLocations({ mine: 'true' }),
-    })
+	const query = useQuery({
+		queryKey: ["locations", "mine"],
+		queryFn: () => fetchLocations({ mine: "true" }),
+	});
 
-    return query
-}
+	return query;
+};
 
 export const useLocation = (id: number) => {
-    const query = useQuery({
-        queryKey: ['locations', id],
-        queryFn: () => fetchLocationDetail(id),
-    })
+	const query = useQuery({
+		queryKey: ["locations", id],
+		queryFn: () => fetchLocationDetail(id),
+	});
 
-    return query
-}
+	return query;
+};
