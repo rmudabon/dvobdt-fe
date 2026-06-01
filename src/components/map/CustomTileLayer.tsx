9a73104593b5976b2fdd7dedc3e4bef35f164bbe
Your Name <you@example.com>
2026-06-01 09:19:18 +0800

    feat: change tilemap

--- src/components/map/CustomTileLayer.tsx
+++ src/components/map/CustomTileLayer.tsx
@@ -1,12 +1,12 @@
-import { BOUNDS } from "@/utils/constants";
 import { TileLayer } from "react-leaflet";
+import { BOUNDS } from "@/utils/constants";
 
 const CustomTileLayer = () => (
-    <TileLayer
-        bounds={BOUNDS}
-        attribution='© OpenStreetMap contributors, © Stadia Maps'
-        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
-    />
-)
+	<TileLayer
+		bounds={BOUNDS}
+		attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+		url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+	/>
+);
 
-export { CustomTileLayer }
+export { CustomTileLayer };
